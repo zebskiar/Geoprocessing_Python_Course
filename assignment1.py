6626bb208bc69f729487d6b409db2c3ece277587
Arne Zebski <arne.zebski@googlemail.com>
2018-04-24 13:28:00 +0200

    first commit

---
+++ assignment1.py
@@ -0,0 +1,38 @@
+# ############################################################################################################# #
+# (c) Matthias Baumann, Humboldt-Universität zu Berlin, 4/23/2018
+# ####################################### LOAD REQUIRED LIBRARIES ############################################# #
+import time
+import ogr
+# ####################################### SET TIME-COUNT ###################################################### #
+starttime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+print("--------------------------------------------------------")
+print("Starting process, time: " + starttime)
+print("")
+# ####################################### FOLDER PATHS & global variables ##################################### #
+#SHP = "L:/_SHARED_DATA/CL_MB/tc_sc/_Version02_300m/points_300m_clip.shp"
+#outputFile = "L:/_SHARED_DATA/CL_MB/tc_sc/_Version02_300m/points_300m_clip_summary.shp"
+#buff_m = 100
+
+
+# ####################################### PROCESSING ########################################################## #
+
+os.listdir(D:/UNI/Python/Assignment01/Assignment01_data/Part01_Landsat)
+
+#files = open(D:\UNI\Python\Assignment01\Assignment01_data\Part01_Landsat)
+
+
+
+
+
+
+
+
+
+# ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
+print("")
+endtime = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+print("--------------------------------------------------------")
+print("--------------------------------------------------------")
+print("start: " + starttime)
+print("end: " + endtime)
+print("")
