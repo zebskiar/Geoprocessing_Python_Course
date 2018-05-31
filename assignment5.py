af97d3ad7c4f227d93a8f563ef95aa7c37d4d108
Arne Zebski <arne.zebski@googlemail.com>
2018-05-31 18:07:57 +0200

    reprojection and random point sampling

--- assignment5.py
+++ assignment5.py
@@ -22,10 +22,15 @@
 # ####################################### LOAD REQUIRED LIBRARIES ############################################# #
 
 import time
-import ogr
-from shapely import geometry
-import random
 import geopandas as gpd
+from osgeo import ogr
+from osgeo import gdal
+from osgeo import osr
+from osgeo import gdal
+import random
+import numpy as np
+from shapely import wkt
+import pandas as pd
 
 # ####################################### SET TIME-COUNT ###################################################### #
 
@@ -36,92 +41,93 @@ print("")
 
 # ####################################### FOLDER PATHS & global variables ##################################### #
 
-wd       = 'D:/Britta/Documents/HU Berlin/SS 18/Geoprocessing with Python/Week 7 - Vector processing II/Assignment05 - data/'
-ger_path = 'gadm36_GERonly.shp'
-pas_path = 'WDPA_May2018_polygons_GER_select10large.shp'
-sp_path  = 'OnePoint.shp'
+wd = "D:/UNI/Python/Assignment05/data/"
 
 # ####################################### PROCESSING ########################################################## #
 
-germany         = ogr.Open(wd + ger_path)
-protected       = ogr.Open(wd + pas_path)
-starting_point  = ogr.Open(wd + sp_path)
+pareas = gpd.read_file(wd+'WDPA_May2018_polygons_GER_select10large.shp')
+pareas_lambert = pareas.to_crs({'init': 'EPSG:3035'})
+pareas_lambert.to_file(wd+"wdpa_reproj.shp")
 
-ger = germany.GetLayer()
-pas = protected.GetLayer()
-sp  = starting_point.GetLayer()
+expoint = gpd.read_file(wd+'OnePoint.shp')
+expoint = expoint.to_crs({'init': 'EPSG:3035'})
+expoint.to_file(wd+"onepoint_reproj.shp")
 
-x_ori = sp.GetExtend()[0]
-y_ori = sp.GetExtend()[2]
+driver = ogr.GetDriverByName('ESRI Shapefile')
+dataSourcePoint = driver.Open(wd+"onepoint_reproj.shp", 0)
+ExPoint = dataSourcePoint.GetLayer()
+ExPointCoord = (ExPoint.GetExtent())
+x_ori = ExPointCoord[0]
+y_ori = ExPointCoord[2]
 
-# FUNCTION to create random points
-'''
-def generate_random(number, polygon):
-    list_of_points = []
-    minx, miny, maxx, maxy = polygon
-    counter = 0
-    while counter < number:
-        pnt = ogr.Geometry(ogr.wkbPoint)
-        pnt.SetPoint_2D(0, random.uniform(minx, maxx), random.uniform(miny, maxy))
-        if geom.Contains(pnt):
-            pnt_list.append(pnt)
-            counter += 1
-    return list_of_points
-'''
+# open reprojected PA shp
+dataSourcePA = driver.Open(wd+'wdpa_reproj.shp', 0)
+pas = dataSourcePA.GetLayer()
+
+# data frame preparation
+ID = 0
+pnt_df = pd.DataFrame(columns=["ID", "PA_NAME", "X_COORD", "Y_COORD"])
 
 # loop through PAs
 for pa in pas:
-    #1) get pa
-    print(pa.GetField('NAME')) #works
+    # 1) get pa
+    pa_name = pa.GetField('NAME')
+    print(pa_name)
 
-    #2) transform to EPSG 3035
-    tmp = gpd.GeoDataFrame.from_file(wd + pas_path)
-    PA_Lambert = tmp.to_crs(epsg=3035)
-    #PA_Lambert.crs #not necessary
-    PA_Lambert.to_file(wd + 'PA_Lambert.shp')
-    PA_Lambert = ogr.Open(wd + 'PA_Lambert.shp')
-    paslam = PA_Lambert.GetLayer() #we need to keep using this, but how do we do this if 'pa' is the loop-element?
+    # 2) get extent of pa
+    geom = pa.GetGeometryRef()
+    pa_ext = geom.GetEnvelope()
 
-    #3) get extent of pa
-    geom = pa.GetGeometryRef()#works
-    pa_ext = geom.GetEnvelope()#works
-
-    #4) create random points a multitude of 30m away from starting point
-    x_min, y_min, x_max, y_max = pa_ext
+    # 3) create random points a multitude of 30m away from starting point
+    x_min, x_max, y_min, y_max = pa_ext
     pnt_list = []
     while len(pnt_list) < 3: #50, 3 as a test
-
         # find range of x coords to draw random number
         seq_x_min = x_ori + (int((x_min - x_ori) / 30)) * 30
         seq_x_max = x_ori + (int((x_max - x_ori) / 30)) * 30
-        x_random = random.choice(range(seq_x_min, seq_x_max, 30))  # generate random x coordinate
+        x_random = random.choice(np.arange(seq_x_min, seq_x_max, 30))  # generate random x coordinate
 
         # find range of y coords to draw random number
         seq_y_min = y_ori + (int((y_min - y_ori) / 30)) * 30
         seq_y_max = y_ori + (int((y_max - y_ori) / 30)) * 30
-        y_random = random.choice(range(seq_y_min, seq_y_max, 30))  # generate random y coordinate
+        y_random = random.choice(np.arange(seq_y_min, seq_y_max, 30))  # generate random y coordinate
 
+        # 4) check if point within borders of PA (not extent) #works
         pnt = ogr.Geometry(ogr.wkbPoint)  # create point class object
         pnt.AddPoint(x_random, y_random)  # add point coordinate
-
-        # 5) check if point within borders of PA (not extent) #works
         if geom.Contains(pnt):
-            pnt_list.append(pnt)
-    print(len(pnt_list)) #check if the while loop worked
-    #print(pnt_list[0]) #check first point per PA
-    x, y = pnt_list[0].GetX(), pnt_list[0].GetY() #how to get coordinates from point
 
+            # 5) check if point is at least 64m from nearest boundary
+            wkt_pa = wkt.loads(str(geom))
+            wkt_pnt = wkt.loads(str(pnt))
+            if wkt_pa.boundary.distance(wkt_pnt) >= 64:
 
-    # The following needs to be included into the function above (generate_random):
-    #6) check if point has min x meters distance to nearest border
-        #find min distance to nearest border --> min_dist
-        #min_dist < 60 (@Arne: Was it 60m?)
+                # 6a) check if point is at least 90m from all other points in x direction
+                if all(abs(x-x_random) >= 90 for x in pnt_df["X_COORD"].tolist()) == True:
+                    pnt_list.append(pnt)
+                    pnt_df.loc[len(pnt_df)+1] = [ID, pa_name, x_random, y_random]
+                    ID += 1
+                    print("point meets all criteria (x)")
 
-    # 7) Check if x-coord. >= 90m
-    #       else check if |y-coord| >= 90m
-    #   --> both false, start again
-    #   --> one TRUE save list1
-    # while loop stopping at 50 entries
+                # 6b) if 6a=False, check if point is at least 90m from all other points in y direction
+                elif all(abs(y-y_random) >= 90 for y in pnt_df["Y_COORD"].tolist()) == True:
+                    pnt_list.append(pnt)
+                    pnt_df.loc[len(pnt_df)+1] = [ID, pa_name, x_random, y_random]
+                    ID += 1
+                    print("point meets all criteria (y)")
+                else:
+                    print("point overlap")
+            else:
+                print("point too close to PA boundary:", round(wkt_pa.boundary.distance(wkt_pnt), 2), "m")
+        else:
+            print("point not contained in PA")
+
+    print(len(pnt_list), "points collected") #check if the while loop worked
+    print("")
+
+# write random point sample to txt file in the wd
+pnt_df.to_csv(wd+"test.txt", index=None, sep=',', mode='a')
+
 
 # ####################################### END TIME-COUNT AND PRINT TIME STATS################################## #
 
